219bb93e2ffe3ee1995ff92774f38fae6f5117d3
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-04-03 17:15:21 -0400

    almost done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 var $body = $ ('body');
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+var $btnCol = $('.btn-collapse-expand');
+var $panel = $('.panel');
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
 
-$body.on('click', '.btn-show-hide', function () {
-	$('.box').addClass('box-show');
+
+$btnShowHide.on('click', function () {
+	$box.toggleClass('box-show');
+});
+
+$btnMove.on('click', function () {
+	$diamond.toggleClass('diamond-move');
+});
+
+$btnCol.on('click', function () {
+	$panel.toggleClass('panel-collapse');
+});
+
+$btnBounce.on('click', function () {
+	$circle.toggleClass('bounce');
+});
+
+$btnAppend.on('click', function () {
+	$list.toggleClass('expand');
 });
 
